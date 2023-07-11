9bdb3ac94659a4028dddd52dc6ece307dc643fd1
amit <amitshaw1421@gmail.com>
2023-07-11 22:53:54 +0530

    vue if else and v-forloop

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import SignUp from './components/Resturant/SignUp';
 import Home from './components/Resturant/Home';
 import LogIn from './components/Resturant/LogIn';
+import Loop from './components/Loop'
 import {createRouter,createWebHistory} from 'vue-router';
 
 const routes=[
@@ -19,6 +20,11 @@ const routes=[
     component:LogIn,
     path:'/login'
 },
+    {
+    name: 'loop',
+    component:Loop,
+    path:'/loop'
+}
 ]
 const router = createRouter({
     history:createWebHistory(),
